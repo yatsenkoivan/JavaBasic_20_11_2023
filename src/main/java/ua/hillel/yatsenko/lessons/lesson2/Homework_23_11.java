3ed9aad00c3399d5e6a6f193d4fdd88b88dc3a58
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-11-27 21:01:59 +0200

    Added files

---
+++ src/main/java/ua/hillel/yatsenko/lessons/lesson2/Homework_23_11.java
@@ -0,0 +1,15 @@
+package ua.hillel.yatsenko.lessons.lesson2;
+
+public class Homework_23_11
+{
+    public static void main(String[] args)
+    {
+        String name = "Vinnytsia, Ukraine";
+        double latitude = 49.233082;
+        double longitude = 28.468218;
+
+        System.out.println(name);
+        System.out.println("Latitude:\t" + latitude);
+        System.out.println("Longitude:\t" + longitude);
+    }
+}
