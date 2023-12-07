b8a41bcbeca3bd15900c1e3caed74b91db13fe20
Ivan Yatsenko <aokiroyal16@gmail.com>
2023-12-07 18:51:07 +0200

    Lesson 5 (fix)

--- src/main/java/ua/hillel/yatsenko/lessons/lesson5/HomeWork.java
+++ src/main/java/ua/hillel/yatsenko/lessons/lesson5/HomeWork.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class HomeWork
 {
+    static void showErrorMessage()
+    {
+        System.out.println("Помилка! Введіть ціле число.");
+        System.exit(0);
+    }
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
@@ -14,38 +19,59 @@ public class HomeWork
         System.out.print("Введіть ім'я другої команди: ");
         team2 = sc.nextLine();
 
-        int team1_frags_1, team1_frags_2, team1_frags_3, team1_frags_4, team1_frags_5;
-        int team2_frags_1, team2_frags_2, team2_frags_3, team2_frags_4, team2_frags_5;
+        int team1_frags_1=0, team1_frags_2=0, team1_frags_3=0, team1_frags_4=0, team1_frags_5=0;
+        int team2_frags_1=0, team2_frags_2=0, team2_frags_3=0, team2_frags_4=0, team2_frags_5=0;
 
         System.out.println("Фраги \"" + team1 + "\":");
+
         System.out.println("Перший гравець: ");
-        team1_frags_1 = sc.nextInt();
+        if (sc.hasNextInt()) team1_frags_1 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Другий гравець: ");
-        team1_frags_2 = sc.nextInt();
+        if (sc.hasNextInt()) team1_frags_2 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Третій гравець: ");
-        team1_frags_3 = sc.nextInt();
+        if (sc.hasNextInt()) team1_frags_3 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Четвертий гравець: ");
-        team1_frags_4 = sc.nextInt();
+        if (sc.hasNextInt()) team1_frags_4 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("П'ятий гравець: ");
-        team1_frags_5 = sc.nextInt();
+        if (sc.hasNextInt()) team1_frags_5 = sc.nextInt();
+        else showErrorMessage();
 
         System.out.println("Фраги \"" + team2 + "\":");
+
         System.out.println("Перший гравець: ");
-        team2_frags_1 = sc.nextInt();
+        if (sc.hasNextInt()) team2_frags_1 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Другий гравець: ");
-        team2_frags_2 = sc.nextInt();
+        if (sc.hasNextInt()) team2_frags_2 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Третій гравець: ");
-        team2_frags_3 = sc.nextInt();
+        if (sc.hasNextInt()) team2_frags_3 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("Четвертий гравець: ");
-        team2_frags_4 = sc.nextInt();
+        if (sc.hasNextInt()) team2_frags_4 = sc.nextInt();
+        else showErrorMessage();
+
         System.out.println("П'ятий гравець: ");
-        team2_frags_5 = sc.nextInt();
+        if (sc.hasNextInt()) team2_frags_5 = sc.nextInt();
+        else showErrorMessage();
+
 
         int team1_total = team1_frags_1 + team1_frags_2 + team1_frags_3 + team1_frags_4 + team1_frags_5;
         int team2_total = team2_frags_1 + team2_frags_2 + team2_frags_3 + team2_frags_4 + team2_frags_5;
 
-        double team1_avg = team1_frags_1/5.0;
-        double team2_avg = team2_frags_1/5.0;
+        double team1_avg = team1_total/5.0;
+        double team2_avg = team2_total/5.0;
 
         String team_winner;
         int result;
